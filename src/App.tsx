8aab253106fd2d423459714e8420fcf9d2cbc3c8
niiima <nima.bastani@gmail.com>
2022-05-26 18:32:28 +0200

    Fix Style and update manifest with name and title

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import { Playlist } from './components/Playlist';
 import ReactPlayer from 'react-player'
 import { useEffect, useState } from 'react';
 function App() {
-  const [playing, setPlaying] = useState('');
-  useEffect(() => {
-    console.log(playing)
-  }, [playing]);
 
   const [scrollPosition, setScrollPosition] = useState(0);
+  const [playing, setPlaying] = useState('');
+
+  useEffect(() => {
+    console.log(playing)
+  }, [playing, scrollPosition]);
+
   const handleScroll = () => {
     const position = window.pageYOffset;
     setScrollPosition(position);
@@ -24,25 +26,23 @@ function App() {
   }, []);
 
   return (
-    <div className="flex flex-col h-screen">
-      <header className={`py-20 bg-gray-700 text-white text-center`}
-      // height={scrollPosition > 40 ? "20%" : "100%"}
-
+    <div className="flex flex-col h-screen bg-stale-800">
+      <header className={`py-0 bg-gray-700 text-white text-center`}
       >
 
         {/* <div className='player-wrapper'> */}
         <ReactPlayer url={playing}
-          className='' width='100%' height='36%'
+          width='100%' height='100%'
         />
         {/* </div> */}
       </header>
-      <main className=" flex-1 overflow-y-auto p-5">
+      <main className="bg-slate-800 flex-1 overflow-y-auto p-5">
         <Playlist array={db} size="300px" setPlayUrl={setPlaying} />
       </main>
       {/* <footer className="py-5 text-size-[1rem] bg-gray-700 text-center text-white">
         Enjoy The playlist
       </footer> */}
-    </div>
+    </div >
   );
 }
 
