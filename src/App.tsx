75983da802caa1162283c4deea561f1a04e46e8a
niiima <nima.bastani@gmail.com>
2022-05-26 18:14:57 +0200

    Update header size and remove footer

--- src/App.tsx
+++ src/App.tsx
@@ -25,22 +25,23 @@ function App() {
 
   return (
     <div className="flex flex-col h-screen">
-      <header className={`py-10 bg-gray-700 text-white text-center`}
+      <header className={`py-20 bg-gray-700 text-white text-center`}
       // height={scrollPosition > 40 ? "20%" : "100%"}
 
       >
 
         {/* <div className='player-wrapper'> */}
         <ReactPlayer url={playing}
-          className='react-player' width='100%' height='20%'
+          className='' width='100%' height='36%'
         />
         {/* </div> */}
       </header>
       <main className=" flex-1 overflow-y-auto p-5">
         <Playlist array={db} size="300px" setPlayUrl={setPlaying} />
       </main>
-      <footer className="py-5 bg-gray-700 text-center text-white">
-      </footer>
+      {/* <footer className="py-5 text-size-[1rem] bg-gray-700 text-center text-white">
+        Enjoy The playlist
+      </footer> */}
     </div>
   );
 }
